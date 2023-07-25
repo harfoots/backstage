97b11202a7390e36f098b5885ff7a5eeaa9f9370
Smith Mathieu <smithm@lyft.com>
2023-07-25 08:26:15 -0700

    Update microsite/src/pages/on-demand/_onDemandCard.tsx:

--- microsite/src/pages/on-demand/_onDemandCard.tsx
+++ microsite/src/pages/on-demand/_onDemandCard.tsx
@@ -1,7 +1,3 @@
-import Link from '@docusaurus/Link';
-import { SimpleCard } from '@site/src/components/simpleCard/simpleCard';
-import React from 'react';
-
 export interface IOnDemandData {
   title: string;
   category: string;
@@ -11,56 +7,5 @@ export interface IOnDemandData {
   youtubeImgUrl: string;
   rsvpUrl: string;
   eventUrl: string;
+  children?: React.ReactNode;
 }
-
-export const OnDemandCard = ({
-  title,
-  category,
-  description,
-  date,
-  youtubeUrl,
-  youtubeImgUrl,
-  rsvpUrl,
-  eventUrl,
-}: IOnDemandData) => (
-  <SimpleCard
-    header={
-      <>
-        <h3>{title}</h3>
-
-        <p className="PluginCardAuthor">on {date}</p>
-
-        <span className="badge badge--primary">{category}</span>
-
-        <img src={youtubeImgUrl} alt={title} />
-      </>
-    }
-    body={<p>{description}</p>}
-    footer={
-      category.toLowerCase() === 'upcoming' ? (
-        <>
-          <Link
-            to={eventUrl}
-            className="button button--outline button--sm button--primary"
-          >
-            Event page
-          </Link>
-
-          <Link
-            to={rsvpUrl}
-            className="button button--outline button--sm button--primary"
-          >
-            Remind me
-          </Link>
-        </>
-      ) : (
-        <Link
-          to={youtubeUrl}
-          className="button button--outline button--primary button--block"
-        >
-          Watch on YouTube
-        </Link>
-      )
-    }
-  />
-);
